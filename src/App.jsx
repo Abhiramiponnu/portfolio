67df467747ad9a06a6406925d4671228381cad15
ct24 <abhirami.b2062005@gmail.com>
2026-05-21 13:53:54 +0530

    Fix duplicate component imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,17 @@
-import React from "react";
-import Navbar from "./componnets/Navbar";
-import Home from "./components/Home";                
-import Home from "./components/About"; 
-import Home from "./components/Education"; 
-import Home from "./components/Skills"; 
-import Home from "./components/Contact"; 
+import Home from "./components/Home";
+import About from "./components/About";
+import Education from "./components/Education";
+import Skills from "./components/Skills";
+import Contact from "./components/Contact";
 function App() {
   return (
-    <div>
-      <Navbar />
-      <section id="home"><h1>Home</h1></section>
-      <section id="about"><h1>About</h1></section>
-      <section id="education"><h1>Education</h1></section>
-      <section id="skills"><h1>Skills</h1></section>
-      <section id="contact"><h1>Contact</h1></section>
-      
-    </div>
+    <>
+      <Home />
+      <About />
+      <Education />
+      <Skills />
+      <Contact />
+    </>
   );
 }
 
